7da75bf3339b2c4f688e065ef9916f2a4c49e090
liw <395383468@qq.com>
2018-04-18 08:45:52 +0800

    first commit

---
+++ src/components/header/header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}}</span>
+        </div>
+        <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-wrapper">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style type="text/stylus" lang="stylus" rel="stylesheet/stylus">
+  .header
+    color: #fff
+    background: #000
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      font-size: 0px
+      .avatar
+        display: inline-block
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size: 14px
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    ratings
+  </div>
+</template>
+
+<script>
+    export default {
+        name: ""
+    }
+</script>
+
+<style scoped>
+
+</style>
